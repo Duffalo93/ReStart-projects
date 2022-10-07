6dd16b23aa13fc17d0f4cc2883a338109c11ad74
James Duffy <112356745+Duffalo93@users.noreply.github.com>
2022-10-07 19:03:55 +1300

    Add files via upload

--- Gym.py
+++ Gym.py
@@ -1,24 +1,12 @@
-# This is a program to choose which order of exercises you will peform!
-
-# This is a list of the body type you will exercise
-#  - Legs , Arms , Shoulders, Chest, Back 
+# Importing the random module.
+import random
 
 # defines back list to choose from
-def back_list():
-    pass
+#def back_list():
+ #   pass
 
 # back workouts within list
-back_list = [
-    "Pull Up"
-    "Bent-Over Row"
-    "Single-Arm Dumbbell Row"
-    "Lat Pulldown"
-    "Seated back row"
-]
-
-# defines arm list to choose from
-def arm_list():
-
+back_list = ["Pull Up", "Bent-Over Row", "Single-Arm Dumbbell Row", "Lat Pulldown", "Seated back row"]
 
 # Ask user for name
 name = input("Kia ora, what is your name? ")
@@ -29,44 +17,11 @@ print ("Kia ora " + name)
 bodypart = input("What body part do you want to workout?")
 print ("Sweet, you will be working out " + bodypart)
 
-# Ask user what their workout goal is
-
-goal = input("What is your workout goal?")
-print (goal + " ")
-
+if bodypart == 'Back':
+    exercise = random.choice(back_list)
+    print(exercise) 
 
 
-# This is the exercises used for the chosen body type
 
-# Back Exercises
-# Pull-Up
-# Bent-Over Row
-# Chest Supported Row
-# Single-Arm Dumbbell Row
-# Inverted Row
-# TRX Suspension Row
-# Lat Pulldown
-# Neutral Grip Pulldown
-# Seated Cable Row 
-# Cable Trap Shrug
-# Cable Rope Pullover
-# Landmine Row
-# Farmer’s Carry
-# Towel Chin-Up
-# Straight-Arm Pulldown
-# Kroc Row
-# Pendlay Row
-# Face Pull
-# Seal Row
-# Meadows Row
-# Back Hyperextension
 
-# Arm Exercises
-# Hammer curl
-# Dip
-# Close-grip Pushup
-# EZ-Bar Preacher Curl
-# Pullover/Triceps Extension
-# Triceps Pushdown
-# Close-grip Bench Press
-# Underhand Kickback
+
